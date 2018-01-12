3423c8867917b8120025341209fa4721d26e8741
Rainyn <2695792010yjk@gmail.com>
2018-01-12 14:57:55 +0800

    增加SQL

---
+++ src/main/resources/SQL/db.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Trial
+MySQL - 5.7.13-log : Database - springbootdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `city` */
+CREATE DATABASE springbootdb;
+CREATE TABLE `city` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '城市编号',
+  `province_id` int(10) unsigned NOT NULL COMMENT '省份编号',
+  `city_name` varchar(25) DEFAULT NULL COMMENT '城市名称',
+  `description` varchar(25) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `city` */
+
+insert  into `city`(`id`,`province_id`,`city_name`,`description`) values (1,1,'杭州','我住在杭州。');
+
+
+CREATE DATABASE springbootdb_cluster;
+CREATE TABLE USER
+(
+id INT(10) UNSIGNED PRIMARY KEY NOT NULL COMMENT '用户编号' AUTO_INCREMENT,
+user_name VARCHAR(25) COMMENT '用户名称',
+description VARCHAR(25) COMMENT '描述'
+)ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+INSERT USER VALUES (1 ,'Rainyn','他是一个大帅哥');
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
